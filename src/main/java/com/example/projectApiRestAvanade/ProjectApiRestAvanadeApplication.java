ad20f74d1ec52b5446b28171956ca6b1795f2679
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-06 19:01:00 -0300

    First commit

---
+++ src/main/java/com/example/projectApiRestAvanade/ProjectApiRestAvanadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectApiRestAvanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestAvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRestAvanadeApplication.class, args);
+	}
+
+}
