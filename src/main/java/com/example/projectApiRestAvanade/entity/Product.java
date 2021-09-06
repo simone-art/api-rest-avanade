d482d132f77ff1bcf6ea33177ca9be22a859f26f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-06 19:48:49 -0300

    Adicionado no arquivo pom a dependência JPA para que o GeneratedValue possa ser importado

--- src/main/java/com/example/projectApiRestAvanade/entity/Product.java
+++ src/main/java/com/example/projectApiRestAvanade/entity/Product.java
@@ -1,4 +1,17 @@
 package com.example.projectApiRestAvanade.entity;
 
-public class Product {
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
+
+
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
+    @GenericGenerator(name="native", strategy = "native")
+    private long codigo;
 }
