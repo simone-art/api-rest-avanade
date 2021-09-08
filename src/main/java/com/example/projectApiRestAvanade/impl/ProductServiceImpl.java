98600ac929e7176febd2f25c404f873622bd6ef0
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 19:38:16 -0300

    Criada classe para implementar a interface ProductService

---
+++ src/main/java/com/example/projectApiRestAvanade/impl/ProductServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.projectApiRestAvanade.impl;
+
+import com.example.projectApiRestAvanade.entity.Product;
+import com.example.projectApiRestAvanade.service.ProductService;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductFromId(Long codigo) {
+        return null;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return null;
+    }
+}
