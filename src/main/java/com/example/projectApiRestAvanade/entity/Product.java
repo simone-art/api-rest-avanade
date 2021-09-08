4b204f0c2c28d475c404d007ae6893ad1a5fca1a
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 19:07:00 -0300

    Criada tabela product com seus atributos

--- src/main/java/com/example/projectApiRestAvanade/entity/Product.java
+++ src/main/java/com/example/projectApiRestAvanade/entity/Product.java
@@ -1,11 +1,16 @@
 package com.example.projectApiRestAvanade.entity;
 
 
+import lombok.Data;
+
 import java.io.Serializable;
 
-
+@Data
 public class Product implements Serializable {
-
-
     private long codigo;
+    private String nome;
+    private int quantidade;
+    private String porte;
+    private String categoria;
+    private String departamento; 
 }
