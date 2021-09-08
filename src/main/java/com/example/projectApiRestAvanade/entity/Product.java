d1fa4ba39b70b59b7ad68032e99a27b571e9b3d6
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 18:58:56 -0300

    Alterado o código para fazer funcionar o MongoDb

--- src/main/java/com/example/projectApiRestAvanade/entity/Product.java
+++ src/main/java/com/example/projectApiRestAvanade/entity/Product.java
@@ -1,17 +1,11 @@
 package com.example.projectApiRestAvanade.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import java.io.Serializable;
 
 
 public class Product implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
-    @GenericGenerator(name="native", strategy = "native")
+
     private long codigo;
 }
