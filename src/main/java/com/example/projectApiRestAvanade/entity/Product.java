37374aff7f9247c790fdb510e522b3e5fc4a0864
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 19:25:00 -0300

    Incluidas anotações @Id e @Document para que o mongoDb funcione

--- src/main/java/com/example/projectApiRestAvanade/entity/Product.java
+++ src/main/java/com/example/projectApiRestAvanade/entity/Product.java
@@ -2,11 +2,16 @@ package com.example.projectApiRestAvanade.entity;
 
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 @Data
+@Document
 public class Product implements Serializable {
+    
+    @Id
     private long codigo;
     private String nome;
     private int quantidade;
