c795e5699dc14c665dcf0c5f36f0982b2fe0ccff
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 19:22:54 -0300

    Alterado código

--- src/main/java/com/example/projectApiRestAvanade/entity/Product.java
+++ src/main/java/com/example/projectApiRestAvanade/entity/Product.java
@@ -3,10 +3,13 @@ package com.example.projectApiRestAvanade.entity;
 
 import lombok.Data;
 
+
 import java.io.Serializable;
 
 @Data
+
 public class Product implements Serializable {
+
     private long codigo;
     private String nome;
     private int quantidade;
