98ebd90a427d0c5076509978042d81a3c4f9263a
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-09-08 19:43:17 -0300

    Feitas alterações

--- src/main/java/com/example/projectApiRestAvanade/impl/ProductServiceImpl.java
+++ src/main/java/com/example/projectApiRestAvanade/impl/ProductServiceImpl.java
@@ -2,12 +2,15 @@ package com.example.projectApiRestAvanade.impl;
 
 import com.example.projectApiRestAvanade.entity.Product;
 import com.example.projectApiRestAvanade.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    
     @Override
     public List<Product> getAllProducts() {
         return null;
